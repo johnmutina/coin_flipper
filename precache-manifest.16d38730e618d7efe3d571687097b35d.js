1ed9f4eb3a3702cd318040f44f70a0982a4aaa79
johnMutina <johnmutina@gmail.com>
2019-07-08 14:57:16 +0200

    Updates

---
+++ precache-manifest.16d38730e618d7efe3d571687097b35d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03f0ed0ef9f80a7ad801fcacb6bea1d",
+    "url": "/coin_flipper/index.html"
+  },
+  {
+    "revision": "97546938da57486b0831",
+    "url": "/coin_flipper/static/css/main.4d9e3660.chunk.css"
+  },
+  {
+    "revision": "aabe391d1ebe3f4eb7e8",
+    "url": "/coin_flipper/static/js/2.8b3e38ad.chunk.js"
+  },
+  {
+    "revision": "97546938da57486b0831",
+    "url": "/coin_flipper/static/js/main.9480e810.chunk.js"
+  },
+  {
+    "revision": "692b6038ff98da0437ec",
+    "url": "/coin_flipper/static/js/runtime~main.4ef5c0a7.js"
+  }
+]);
